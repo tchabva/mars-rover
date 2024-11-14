708a7fc3a4f9a0c2929b1de0a43839bf57e60564
Tawanda Chabva <tchabva@gmail.com>
2024-11-14 15:24:42 +0000

    feat: added the logic to pass the tests for the PlateauSizeParser

--- src/main/java/org/northcoders/inputlayer/inputparsers/PlateauSizeParser.java
+++ src/main/java/org/northcoders/inputlayer/inputparsers/PlateauSizeParser.java
@@ -1,14 +1,46 @@
 package org.northcoders.inputlayer.inputparsers;
 
+import org.northcoders.inputlayer.Instruction;
 import org.northcoders.inputlayer.PlateauSize;
 
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PlateauSizeParser {
-    String plateauSizeString;
-    int[] plateauSizeArray = new int[2];
+    private boolean isValidPlateauDimensions;
 
-    /*
-    TODO: takes an input string of the form "5 5" and then convert to an two index intArray that can be used to
-        to initialise PlateauSize Object
-     */
+    public PlateauSize parsePlateauSize(String input){
+        if (input == null){
+            System.out.println("Please enter a valid input!");
+            isValidPlateauDimensions = false;
+            return new PlateauSize(0,0);
+        } else if (!isValidString.test(input)) {
+            System.out.println("Please enter a valid input!");
+            isValidPlateauDimensions = false;
+            return new PlateauSize(0,0);
+        }else {
+            String[] inputArray = input.split(" ");
+            try {
+                int x = Integer.parseInt(inputArray[0]);
+                int y = Integer.parseInt(inputArray[1]);
+                isValidPlateauDimensions = true;
+                return new PlateauSize(x, y);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid input!");
+                isValidPlateauDimensions = false;
+                return new PlateauSize(0,0);
+            }
+        }
+    }
 
+    private final Predicate<String> isValidString = input ->{
+        Pattern pattern = Pattern.compile("(\\d+) (\\d+)");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    };
+
+    public boolean isValidPlateauDimensions() {
+        return isValidPlateauDimensions;
+    }
 }
