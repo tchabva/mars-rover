0c7baa85ab233e9c397b1096caaa5995ba22be28
Tawanda Chabva <tchabva@gmail.com>
2024-11-14 11:05:18 +0000

    feat: created PositionParser class with TODO notes for methods

---
+++ src/main/java/org/northcoders/inputlayer/inputparsers/PositionParser.java
@@ -0,0 +1,12 @@
+package org.northcoders.inputlayer.inputparsers;
+
+import org.northcoders.inputlayer.Position;
+
+public class PositionParser {
+    private String positionInputString;
+    private Position position;
+
+    /*
+    TODO: Takes in an input and returns a Position object.
+     */
+}
