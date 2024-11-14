2061dbbd292697d7d0a18108590724b80136191d
Tawanda Chabva <tchabva@gmail.com>
2024-11-14 12:24:55 +0000

    feat: implemented conditons for InputParser to pass all the tests

--- src/main/java/org/northcoders/inputlayer/inputparsers/InstructionParser.java
+++ src/main/java/org/northcoders/inputlayer/inputparsers/InstructionParser.java
@@ -2,10 +2,45 @@ package org.northcoders.inputlayer.inputparsers;
 
 import org.northcoders.inputlayer.Instruction;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InstructionParser {
     private String instructionString;
-    private Instruction instruction;
+    private String[] instructionStringArray;
+    private Queue<Instruction> instructions = new LinkedList<>();
+    private boolean isValidInstruction;
 
     // TODO: create method to accept an input string, parses the input string to an ArrayList of instructions
 
+    public Queue<Instruction> parseInstructions(String input) {
+        if (input == null){
+            System.out.println("Please enter a valid Instruction!");
+            isValidInstruction = false;
+            return null;
+        }else if (input.equals("")){
+            System.out.println("No instructions submitted!");
+            isValidInstruction = false;
+            return null;
+        } else {
+            instructionStringArray = input.split("");
+
+            for (String inst : instructionStringArray){
+                try{
+                    instructions.add(Instruction.valueOf(inst.toUpperCase()));
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Please enter a valid Instruction!");
+                    isValidInstruction = false;
+                    return null;
+                }
+            }
+        }
+        isValidInstruction = true;
+        return instructions;
+    }
+
+    public boolean isValidInstruction() {
+        return isValidInstruction;
+    }
 }
