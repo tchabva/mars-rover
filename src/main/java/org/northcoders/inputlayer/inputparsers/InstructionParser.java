af4fecc4cf962b54d3b09abd83fbbae54af4451b
Tawanda Chabva <tchabva@gmail.com>
2024-11-14 10:47:31 +0000

    feat: created InstructionParser class with TODO notes for methods

---
+++ src/main/java/org/northcoders/inputlayer/inputparsers/InstructionParser.java
@@ -0,0 +1,11 @@
+package org.northcoders.inputlayer.inputparsers;
+
+import org.northcoders.inputlayer.Instruction;
+
+public class InstructionParser {
+    private String instructionString;
+    private Instruction instruction;
+
+    // TODO: create method to accept an input string, parses the input string to an ArrayList of instructions
+
+}
