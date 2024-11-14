9e89d7d301e8aeb73e9c30495cae3ea06a044946
Tawanda Chabva <tchabva@gmail.com>
2024-11-14 22:06:40 +0000

    feat: added Position attribute with setter and getter;

--- src/main/java/org/northcoders/logiclayer/Rover.java
+++ src/main/java/org/northcoders/logiclayer/Rover.java
@@ -1,4 +1,20 @@
 package org.northcoders.logiclayer;
 
+import org.northcoders.inputlayer.Position;
+
 public class Rover {
+    private Position position;
+
+
+    public Rover(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
