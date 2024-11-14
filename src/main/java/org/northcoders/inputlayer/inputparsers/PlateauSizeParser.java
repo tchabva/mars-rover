b607002d206d95b5e9c4953554c782582ab9c91c
Tawanda Chabva <tchabva@gmail.com>
2024-11-14 10:59:23 +0000

    feat: created PlateauSizeParser class with TODO notes for methods

---
+++ src/main/java/org/northcoders/inputlayer/inputparsers/PlateauSizeParser.java
@@ -0,0 +1,14 @@
+package org.northcoders.inputlayer.inputparsers;
+
+import org.northcoders.inputlayer.PlateauSize;
+
+public class PlateauSizeParser {
+    String plateauSizeString;
+    int[] plateauSizeArray = new int[2];
+
+    /*
+    TODO: takes an input string of the form "5 5" and then convert to an two index intArray that can be used to
+        to initialise PlateauSize Object
+     */
+
+}
