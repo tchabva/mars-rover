2d7c873b86cc9f3320e33060b7a1fc12c8ab89be
Tawanda Chabva <tchabva@gmail.com>
2024-11-15 14:25:55 +0000

    feat: created test for the Plateau class

---
+++ src/test/java/org/northcoders/logiclayer/PlateauTest.java
@@ -0,0 +1,105 @@
+package org.northcoders.logiclayer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.northcoders.inputlayer.CompassDirection;
+import org.northcoders.inputlayer.PlateauSize;
+import org.northcoders.inputlayer.Position;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlateauTest {
+
+    @Test
+    @DisplayName("Returns true if x and y are within the plateau boundary")
+    void testReturnsTrueIfXandYAreInPlateau(){
+        // Arrange
+        Plateau plateau = new Plateau(new PlateauSize(10, 10));
+
+        // Act
+        boolean result = plateau.isValidPosition.test(1,1);
+        boolean resultTwo = plateau.isValidPosition.test(11,11);
+        boolean resultThree = plateau.isValidPosition.test(5,1);
+        boolean resultFour = plateau.isValidPosition.test(0,0);
+
+        // Assert
+        assertAll("Return true as they are in all the plateau bounds",
+                () -> assertTrue(result),
+                () -> assertTrue(resultTwo),
+                () -> assertTrue(resultThree),
+                () -> assertTrue(resultFour)
+        );
+    }
+
+    @Test
+    @DisplayName("Returns false if x or y are not within the plateau boundary")
+    void testReturnsFalseIfXOrYAreNotInPlateauBoundary(){
+        // Arrange
+        Plateau plateau = new Plateau(new PlateauSize(10, 10));
+
+        // Act
+        boolean result = plateau.isValidPosition.test(12,1);
+        boolean resultTwo = plateau.isValidPosition.test(11,21);
+        boolean resultThree = plateau.isValidPosition.test(-1,1);
+        boolean resultFour = plateau.isValidPosition.test(12,12);
+
+        // Assert
+        assertAll("Return false as they are not all in the plateau bounds",
+                () -> assertFalse(result),
+                () -> assertFalse(resultTwo),
+                () -> assertFalse(resultThree),
+                () -> assertFalse(resultFour)
+        );
+    }
+
+    @Test
+    @DisplayName("Returns true if x and y are within the are not coordinates of a Rover")
+    void testReturnsTrueIfXandYAreNotCoordinatesOfARover(){
+        // Arrange
+        Plateau plateau = new Plateau(new PlateauSize(10, 10));
+
+        plateau.getRoverList().add(new Rover(new Position(5,5, CompassDirection.W)));
+
+        // Act
+        boolean result = plateau.isPositionFree.test(1,1);
+        boolean resultTwo = plateau.isPositionFree.test(11,11);
+        boolean resultThree = plateau.isPositionFree.test(5,1);
+        boolean resultFour = plateau.isPositionFree.test(0,0);
+
+        // Assert
+        assertAll("Return true as they are in all the not Rover coordinates",
+                () -> assertTrue(result),
+                () -> assertTrue(resultTwo),
+                () -> assertTrue(resultThree),
+                () -> assertTrue(resultFour)
+        );
+    }
+
+    @Test
+    @DisplayName("Returns true if x and y are within the are not coordinates of a Rover")
+    void testReturnsFalseIfXandYAreCoordinatesOfARover(){
+        // Arrange
+        Plateau plateau = new Plateau(new PlateauSize(10, 10));
+        plateau.getRoverList().add(new Rover(new Position(5,1, CompassDirection.W)));
+        plateau.getRoverList().add(new Rover(new Position(1,1, CompassDirection.W)));
+        plateau.getRoverList().add(new Rover(new Position(11,11, CompassDirection.W)));
+        plateau.getRoverList().add(new Rover(new Position(0,0, CompassDirection.W)));
+
+        // Act
+        boolean result = plateau.isPositionFree.test(1,1);
+        boolean resultTwo = plateau.isPositionFree.test(11,11);
+        boolean resultThree = plateau.isPositionFree.test(5,1);
+        boolean resultFour = plateau.isPositionFree.test(0,0);
+
+        // Assert
+        assertAll("Return false as they are in all the Rover coordinates bounds",
+                () -> assertFalse(result),
+                () -> assertFalse(resultTwo),
+                () -> assertFalse(resultThree),
+                () -> assertFalse(resultFour)
+        );
+    }
+
+
+
+}
