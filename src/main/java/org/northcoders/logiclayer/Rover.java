8f51cd927a160f6b2e89fa19e0beaa3dcc218e3e
Tawanda Chabva <tchabva@gmail.com>
2024-11-15 09:37:34 +0000

    feat: created rotateCompassDirection BiFunction in the Rover class

--- src/main/java/org/northcoders/logiclayer/Rover.java
+++ src/main/java/org/northcoders/logiclayer/Rover.java
@@ -1,6 +1,11 @@
 package org.northcoders.logiclayer;
 
+import org.northcoders.inputlayer.CompassDirection;
+import org.northcoders.inputlayer.Instruction;
 import org.northcoders.inputlayer.Position;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Rover {
     private Position position;
@@ -16,4 +21,40 @@ public class Rover {
     public void setPosition(Position position) {
         this.position = position;
     }
+
+    public Position nextPosition(Instruction instruction){
+        int x = position.getX();
+        int y = position.getY();
+        CompassDirection compassDirection = position.getFacing();
+
+
+
+        return new Position(x,y, compassDirection);
+    }
+
+    // Rotates the compass direction when given an Instruction of R/L and a CompassDirection
+    private BiFunction<CompassDirection, Instruction, CompassDirection> rotateCompassDirection = (compassDirection, instruction) -> {
+
+        if (compassDirection.equals(CompassDirection.N) && instruction.equals(Instruction.R)){
+            return CompassDirection.E;
+        }else if (compassDirection.equals(CompassDirection.E) && instruction.equals(Instruction.R)){
+            return CompassDirection.S;
+        }else if (compassDirection.equals(CompassDirection.S) && instruction.equals(Instruction.R)) {
+            return CompassDirection.W;
+        }else if (compassDirection.equals(CompassDirection.W) && instruction.equals(Instruction.R)) {
+            return CompassDirection.N;
+        }else if (compassDirection.equals(CompassDirection.W) && instruction.equals(Instruction.L)) {
+            return CompassDirection.S;
+        }else if (compassDirection.equals(CompassDirection.S) && instruction.equals(Instruction.L)) {
+            return CompassDirection.E;
+        }else if (compassDirection.equals(CompassDirection.E) && instruction.equals(Instruction.L)) {
+            return CompassDirection.N;
+        }else if (compassDirection.equals(CompassDirection.N) && instruction.equals(Instruction.L)) {
+            return CompassDirection.W;
+        } else  {
+            return null;
+        }
+    };
+
+
 }
