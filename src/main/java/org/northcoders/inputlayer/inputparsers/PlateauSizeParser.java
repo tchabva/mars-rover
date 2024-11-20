ae126ff8d8c477fb5e341e630dbe72c685060630
Tawanda Chabva <tchabva@gmail.com>
2024-11-20 22:17:25 +0000

    feat: updated error string message

--- src/main/java/org/northcoders/inputlayer/inputparsers/PlateauSizeParser.java
+++ src/main/java/org/northcoders/inputlayer/inputparsers/PlateauSizeParser.java
@@ -8,27 +8,29 @@ import java.util.regex.Pattern;
 
 public class PlateauSizeParser {
     private boolean isValidPlateauDimensions;
-
+// TODO Update testing
     public PlateauSize parsePlateauSize(String input){
+        String retryString = "Try again! Please enter a valid plateau size input:";
+
         if (input == null){
-            System.out.println("Please enter a valid input!");
+            System.out.println(retryString);
             isValidPlateauDimensions = false;
-            return new PlateauSize(0,0);
+            return new PlateauSize(-1,-1);
         } else if (!isValidString.test(input)) {
-            System.out.println("Please enter a valid input!");
+            System.out.println(retryString);
             isValidPlateauDimensions = false;
-            return new PlateauSize(0,0);
+            return new PlateauSize(-1,-1);
         }else {
             String[] inputArray = input.split(" ");
             try {
                 int x = Integer.parseInt(inputArray[0]);
                 int y = Integer.parseInt(inputArray[1]);
-                isValidPlateauDimensions = x != 0 && y != 0;
+                isValidPlateauDimensions = x >= 0 && y >= 0;
                 return new PlateauSize(x, y);
             } catch (NumberFormatException e) {
-                System.out.println("Please enter a valid input!");
+                System.out.println(retryString);
                 isValidPlateauDimensions = false;
-                return new PlateauSize(0,0);
+                return new PlateauSize(-1,-1);
             }
         }
     }
