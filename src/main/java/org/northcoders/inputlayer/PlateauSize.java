8d7cc75c481608e81cf2bfa654f9839acc0ea2c1
Tawanda Chabva <tchabva@gmail.com>
2024-11-20 22:14:44 +0000

    refactor: changed PlateauSize to record

--- src/main/java/org/northcoders/inputlayer/PlateauSize.java
+++ src/main/java/org/northcoders/inputlayer/PlateauSize.java
@@ -1,19 +1,4 @@
 package org.northcoders.inputlayer;
 
-public class PlateauSize {
-    private final int x;
-    private final int y;
-
-    public PlateauSize(int x, int y) {
-        this.y = y;
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getX() {
-        return x;
-    }
+public record PlateauSize(int x, int y) {
 }
