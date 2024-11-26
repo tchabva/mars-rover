774f3efbd4d739ed24bd8170bbe7cd9e7fc09b93
Tawanda Chabva <tchabva@gmail.com>
2024-11-26 22:12:51 +0000

    refactor: Position class to a Record

--- src/main/java/org/northcoders/inputlayer/Position.java
+++ src/main/java/org/northcoders/inputlayer/Position.java
@@ -1,37 +1,3 @@
 package org.northcoders.inputlayer;
 
-public class Position  {
-    private int x;
-    private int y;
-    private CompassDirection facing;
-
-    public Position(int x,  int y,CompassDirection facing) {
-        this.facing = facing;
-        this.y = y;
-        this.x = x;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public CompassDirection getFacing() {
-        return facing;
-    }
-
-    public void setFacing(CompassDirection facing) {
-        this.facing = facing;
-    }
-}
+public record Position(int x, int y, CompassDirection facing)  {}
