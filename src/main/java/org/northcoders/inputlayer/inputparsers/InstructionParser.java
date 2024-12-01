213612767cef8d5e9d00b0df7b55c57b70ec120f
Tawanda Chabva <tchabva@gmail.com>
2024-12-01 22:54:26 +0000

    refactor: moved return statement in InstructionParster

--- src/main/java/org/northcoders/inputlayer/inputparsers/InstructionParser.java
+++ src/main/java/org/northcoders/inputlayer/inputparsers/InstructionParser.java
@@ -34,9 +34,9 @@ public class InstructionParser {
                     return null;
                 }
             }
+            isValidInstruction = true;
+            return instructions;
         }
-        isValidInstruction = true;
-        return instructions;
     }
 
     public boolean isValidInstruction() {
