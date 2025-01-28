adc8b1ec1994f1a835fd5e6d7420595d9cb4c1ed
Tawanda Chabva <tchabva@gmail.com>
2025-01-28 21:23:09 +0000

    feat: implement the UI for one Rover input

--- src/main/java/org/northcoders/Main.java
+++ src/main/java/org/northcoders/Main.java
@@ -1,5 +1,6 @@
 package org.northcoders;
 
+import org.northcoders.logiclayer.MissionControl;
 import org.northcoders.userinputlayer.UserInterface;
 
 public class Main {
@@ -10,5 +11,20 @@ public class Main {
         userInterface.getPlateauInput();
 
         userInterface.getPositionAndInstructions();
+
+        MissionControl missionControl = new MissionControl(
+                userInterface.getPlateauSize(),
+                userInterface.getInstructionsQueueList(),
+                userInterface.getPositions()
+        );
+
+        System.out.println("\nThe initial position(s) of your Rovers");
+        missionControl.printRoverPosition();
+
+        System.out.println("\nMoving the Rovers...");
+        missionControl.moveRoverPosition();
+
+        System.out.println("\nThe final position(s) of your Rovers");
+        missionControl.printRoverPosition();
     }
 }
